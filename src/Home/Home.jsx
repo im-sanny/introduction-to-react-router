7f804c4bc18724c92bcc6db88e861fb382151a10
im-sanny <raselparvezsanny@gmail.com>
2024-03-20 11:56:34 +0600

    added nested route, load data with loader and useLoaderData, dynamic route with params and load single data

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,4 +1,5 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
 
 
@@ -6,8 +7,8 @@ const Home = () => {
     return (
         <div>
             <Header></Header>
-            <h1>This is Home component</h1>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,32 +1,44 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import Home from './Home/Home.jsx';
-import About from './About/About.jsx';
-import Contact from './Contact/Contact.jsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./Home/Home.jsx";
+import About from "./About/About.jsx";
+import Contact from "./Contact/Contact.jsx";
+import Users from "./Users/Users.jsx";
+import UserDetails from "./UserDetails/UserDetails.jsx";
 
-const router =createBrowserRouter([
+const router = createBrowserRouter([
   {
     path: "/",
     element: <Home></Home>,
     children: [
-      {path: '/about',
-      element: <About></About>}
-    ]
+      {
+        path: "/about",
+        element: <About></About>,
+      },
+      {
+        path: "/contact",
+        element: <Contact></Contact>,
+      },
+      {
+        path: "/users",
+        loader: () => fetch('https://jsonplaceholder.typicode.com/users'),
+        element: <Users></Users>
+      },
+      {
+        path: '/user/:userId',
+        loader: ({params}) => fetch(`https://jsonplaceholder.typicode.com/users/${params.userId}`),
+        element: <UserDetails></UserDetails>
+      }
+    ],
+    
   },
-  {
-    path: '/contact',
-    element: <Contact></Contact>
-  }
-])
+  
+]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router}></RouterProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
